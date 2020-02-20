2500aaaddf3262855de17ef75716f999441af7d8
Marcos Vieira <mavbvieira@gmail.com>
2020-02-20 07:46:20 -0300

    [BaseCredito] Tratamento de valores faltantes

--- PreProcessamento.py
+++ PreProcessamento.py
@@ -24,3 +24,15 @@ base.drop(base[base.age < 0].index, inplace = True)
 base['age'].mean()
 base['age'][base.age > 0].mean()
 base.loc[base.age < 0, 'age'] = 40.92
+
+
+pd.isnull(base['age'])
+base.loc[pd.isnull(base['age'])]
+previsores = base.iloc[:, 1:4].values
+classe = base.iloc[:, 4].values
+
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer()
+imputer = imputer.fit(previsores[:, 0:3])
+previsores[:, 0:3] = imputer.transform(previsores[:, 0:3])
